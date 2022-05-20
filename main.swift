22f262c52e0b0262fd3624b085932b51550bff3c
kazukisakai <101170572+kazukisakai823@users.noreply.github.com>
2022-05-20 19:52:25 +0900

    atcoderのABC248A問題の解答例

---
+++ main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  ABC248
+//
+//  Created by 酒井和樹 on 2022/04/16.
+//
+
+import Foundation
+let a = readLine()!
+if a.contains("0") == false{
+    print("0")
+}else if a.contains("1") == false{
+    print("1")
+}else if a.contains("2") == false{
+    print("2")
+}else if a.contains("3") == false{
+    print("3")
+}else if a.contains("4") == false{
+    print("4")
+}else if a.contains("5") == false{
+    print("5")
+}else if a.contains("6") == false{
+    print("6")
+}else if a.contains("7") == false{
+    print("7")
+}else if a.contains("8") == false{
+    print("8")
+}else if a.contains("9") == false{
+    print("9")
+}
